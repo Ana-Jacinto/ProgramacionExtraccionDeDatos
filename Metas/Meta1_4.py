34962619c4eb0d4d3b4a45845a34583389dd4cca
Ana-Jacinto <ana.bjacintoc@gmail.com>
2023-09-17 19:50:02 -0700

    Meta 1.4 Modificaciones

--- Metas/Meta1_4.py
+++ Metas/Meta1_4.py
@@ -8,54 +8,61 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import pandas as pd
 
-def search_amazon(product, pages):
+
+def configure_driver():
     s = Service(ChromeDriverManager().install())
     opc = Options()
     opc.add_argument("--window-size = 1020, 1200")
+    return webdriver.Chrome(service=s, options=opc)
 
-    navegador = webdriver.Chrome(service=s, options=opc)
-    navegador.get("https://www.amazon.com.mx/")
+
+def search_amazon_product(driver, product):
+    driver.get("https://www.amazon.com.mx/")
     time.sleep(10)
 
-    buscador = navegador.find_element(By.NAME, "field-keywords")
+    buscador = driver.find_element(By.NAME, "field-keywords")
     buscador.send_keys(product)
     time.sleep(3)
 
-    btnsearch = navegador.find_element(By.ID, "nav-search-submit-button")
+    btnsearch = driver.find_element(By.ID, "nav-search-submit-button")
     btnsearch.click()
     time.sleep(3)
 
-    # Recorrer el número de páginas especificado
-    for i in range(pages):
-        btnnext = navegador.find_element(By.CSS_SELECTOR, ".s-pagination-next")
+
+def scrape_product_results(driver, pages):
+    data = {"Producto": [], "Raiting": [], "Precio": [], "Fecha de Entrega": []}
+
+    for _ in range(pages):
+        btnnext = driver.find_element(By.CSS_SELECTOR, ".s-pagination-next")
         btnnext.click()
         time.sleep(3)
 
-    soup = BeautifulSoup(navegador.page_source, "html.parser")
-    lista_divs = soup.find_all(name="div", attrs={"class": "a-section a-spacing-small puis-padding-left-small puis-padding-right-small"})
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        lista_divs = soup.find_all(name="div", attrs={
+            "class": "a-section a-spacing-small puis-padding-left-small puis-padding-right-small"})
 
-    data = {"Producto": [], "Raiting": [], "Precio": [], "Fecha de Entrega": []}
+        for i in lista_divs:
+            productname = i.find("span", attrs={"class": "a-size-base-plus a-color-base a-text-normal"})
+            raiting = i.find("span", attrs={"class": "a-size-base puis-bold-weight-text"})
+            price = i.find("span", attrs={"a-price-whole"})
+            date = i.find("span", attrs={"a-color-base a-text-bold"})
 
-    for i in lista_divs:
-        productname = i.find("span", attrs={"class": "a-size-base-plus a-color-base a-text-normal"})
-        raiting = i.find("span", attrs={"class": "a-size-base puis-bold-weight-text"})
-        price = i.find("span", attrs={"a-price-whole"})
-        date = i.find("span", attrs={"a-color-base a-text-bold"})
+            data["Producto"].append(productname.text)
+            data["Raiting"].append(raiting.text if raiting else 0)
+            data["Precio"].append(price.text if price else 0)
+            data["Fecha de Entrega"].append(date.text if date else "Sin Fecha de Entrega")
 
-        data["Producto"].append(productname.text)
-        data["Raiting"].append(raiting.text if raiting else 0)
-        data["Precio"].append(price.text if price else 0)
-        data["Fecha de Entrega"].append(date.text if date else "Sin Fecha de Entrega")
+    return pd.DataFrame(data)
 
-    data_df = pd.DataFrame(data)
-    data_df.to_csv("amazon.csv")
+def main():
+    product = str(input("Ingrese el producto que quiere buscar: "))
+    pages = int(input("¿Cuántas páginas de resultados desea recorrer? "))
 
-    navegador.quit()
+    driver = configure_driver()
+    search_amazon_product(driver, product)
+    datadf = scrape_product_results(driver, pages)
+    datadf.to_csv("amazon.csv")
 
-# Llama a la función con los valores deseados
-product = str(input("Ingrese el producto que quiere buscar:"))
-pages = int(input("¿Cuántas páginas de resultados desea recorrer?"))
+    driver.quit()
 
-search_amazon(product, pages)
-#print(data)
-
+main()
